d89bd3840f0f58b0432e0a6c90113a28ec9d67a5
JoleneKearse <pharaohnutz@gmail.com>
2024-07-17 11:15:04 -0400

    chore: add IntroHeading

---
+++ client/src/components/IntroHeading.tsx
@@ -0,0 +1,17 @@
+export function Header() {
+  return (
+    <header className="px-2 text-center md:px-6 lg:px-20">
+      <p className="text-3xl leading-loose">Hi! I'm</p>
+      <h1 className="block text-6xl subpixel-antialiased font-bold leading-12 text-neutral-alpha bg-gradient-to-br from-pink-600 via-purple-500 to-purple-400 bg-clip-text">
+        Jolene Kearse
+      </h1>
+      <h2 className="block text-4xl leading-tight text-neutral-400">
+        Software Engineer
+      </h2>
+
+      <p className="mt-10 text-2xl leading-snug">
+        I build responsive, creative, & engaging web experiences.
+      </p>
+    </header>
+  );
+}
