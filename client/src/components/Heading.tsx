45fe6559dc0c039d6d1e153689e410135741d005
JoleneKearse <pharaohnutz@gmail.com>
2024-07-17 11:16:49 -0400

    chore: add Heading

---
+++ client/src/components/Heading.tsx
@@ -0,0 +1,9 @@
+export function Header() {
+  return (
+    <header className="px-2 text-center md:px-6 lg:px-20">
+      <h1 className="block text-6xl subpixel-antialiased font-bold leading-12 text-neutral-alpha bg-gradient-to-br from-pink-600 via-purple-500 to-purple-400 bg-clip-text">
+        Meet Jolene
+      </h1>
+    </header>
+  );
+}
