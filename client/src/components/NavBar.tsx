e7400aecb975190c0fbb2de3bc7569e4ecd5d480
JoleneKearse <pharaohnutz@gmail.com>
2024-07-25 17:39:05 -0400

    chore: update navbar to have background blur

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -10,10 +10,10 @@ type NavBarProps = {
 
 export function NavBar({ isHamburgerOpen, setIsHamburgerOpen }: NavBarProps) {
   return (
-    <nav className="fixed top-12 z-25 blur-content">
-      <div className="flex items-center justify-between w-full">
-        <img src={Logo} alt="logo" className="fixed left-0 w-10 md:w-12" />
-        <div className={`md:hidden fixed right-0 ${isHamburgerOpen ? "hidden" : "block"}`}>
+    <nav className="fixed top-0 w-5/6 h-24 bg-purple-950a backdrop-blur-2xl">
+      <div className="flex items-center justify-between w-full h-12">
+        <img src={Logo} alt="logo" className="w-10 md:w-12" />
+        <div className={`md:hidden ${isHamburgerOpen ? "hidden" : "block"}`}>
           <button
             onClick={() => setIsHamburgerOpen(!isHamburgerOpen)}
             className=""
@@ -21,49 +21,49 @@ export function NavBar({ isHamburgerOpen, setIsHamburgerOpen }: NavBarProps) {
             <RxHamburgerMenu size="2em" />
           </button>
         </div>
-      </div>
-      <div
-        className={`flex gap-4 md:gap-6 lg:gap-10 ${
-          isHamburgerOpen
-            ? "block bg-neutral-950 w-screen h-screen flex-col justify-center items-center"
-            : "hidden"
-        } md:flex`}
-      >
-        <TextLink
-          text="Home"
-          tabIndex={1}
-          isHamburgerOpen={isHamburgerOpen}
-          setIsHamburgerOpen={setIsHamburgerOpen}
-        />
-        <TextLink
-          text="About"
-          tabIndex={2}
-          isHamburgerOpen={isHamburgerOpen}
-          setIsHamburgerOpen={setIsHamburgerOpen}
-        />
-        <TextLink
-          text="Skills"
-          tabIndex={3}
-          isHamburgerOpen={isHamburgerOpen}
-          setIsHamburgerOpen={setIsHamburgerOpen}
-        />
-        <TextLink
-          text="Projects"
-          tabIndex={4}
-          isHamburgerOpen={isHamburgerOpen}
-          setIsHamburgerOpen={setIsHamburgerOpen}
-        />
-        <TextLink
-          text="Contact"
-          tabIndex={5}
-          isHamburgerOpen={isHamburgerOpen}
-          setIsHamburgerOpen={setIsHamburgerOpen}
-        />
-        {isHamburgerOpen && (
-          <button onClick={() => setIsHamburgerOpen(!isHamburgerOpen)}>
-            <IoCloseCircleOutline size="2em" />
-          </button>
-        )}
+        <div
+          className={`flex gap-4 md:gap-6 lg:gap-10 ${
+            isHamburgerOpen
+              ? "block bg-neutral-950 w-screen h-screen flex-col justify-center items-center"
+              : "hidden"
+          } md:flex`}
+        >
+          <TextLink
+            text="Home"
+            tabIndex={1}
+            isHamburgerOpen={isHamburgerOpen}
+            setIsHamburgerOpen={setIsHamburgerOpen}
+          />
+          <TextLink
+            text="About"
+            tabIndex={2}
+            isHamburgerOpen={isHamburgerOpen}
+            setIsHamburgerOpen={setIsHamburgerOpen}
+          />
+          <TextLink
+            text="Skills"
+            tabIndex={3}
+            isHamburgerOpen={isHamburgerOpen}
+            setIsHamburgerOpen={setIsHamburgerOpen}
+          />
+          <TextLink
+            text="Projects"
+            tabIndex={4}
+            isHamburgerOpen={isHamburgerOpen}
+            setIsHamburgerOpen={setIsHamburgerOpen}
+          />
+          <TextLink
+            text="Contact"
+            tabIndex={5}
+            isHamburgerOpen={isHamburgerOpen}
+            setIsHamburgerOpen={setIsHamburgerOpen}
+          />
+          {isHamburgerOpen && (
+            <button onClick={() => setIsHamburgerOpen(!isHamburgerOpen)}>
+              <IoCloseCircleOutline size="2em" />
+            </button>
+          )}
+        </div>
       </div>
     </nav>
   );
