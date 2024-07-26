907b883b4d2f3f8adb7b2357d1aee2e51f48e55f
JoleneKearse <pharaohnutz@gmail.com>
2024-07-26 16:55:53 -0400

    chore: fix homepage spacing

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -15,7 +15,7 @@ export function HomePage() {
             className="w-40 mr-20 h-42"
           />
         </div>
-        <div className="mt-20 text-balance md:mt-0 md:flex md:flex-col md:justify-center md:gap-4">
+        <div className="mt-20 space-y-6 text-balance md:mt-0 md:flex md:flex-col md:justify-center md:gap-4">
           <p>I'm a Software Engineer based in Canada specializing in <span className="tech-text">React</span>, <span className="tech-text">JavaScript</span>, and <span className="tech-text">TypeScript</span>.</p>
           <p className="">Check out my <NavLink to="/skills" className="emphasis-text">Skills</NavLink> to see how I can best help you.</p>
           <p>Or explore my <NavLink to="/projects" className="emphasis-text">Projects</NavLink> to see I know my stuff.</p>
