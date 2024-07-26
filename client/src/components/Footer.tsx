e478b60ceb4a3ccafa08526cdd547b0dae149d81
JoleneKearse <pharaohnutz@gmail.com>
2024-07-26 17:25:10 -0400

    fixes #3: get footer to align with navbar on bigger screens

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -4,32 +4,34 @@ import { MdEmail } from "react-icons/md";
 
 export function Footer() {
   return (
-    <footer className="absolute flex justify-between bottom-2">
-      <p className="pr-2 text-sm text-center text-neutral-300">
-        &copy; {new Date().getFullYear()} Meet Jolene. All rights reserved.
-      </p>
-      <ul className="flex gap-2">
-        <SocialLink
-          img={<FaGithub />}
-          title="Github"
-          url="https://github.com/JoleneKearse/"
-        />
-        <SocialLink
-          img={<FaLinkedin />}
-          title="LinkedIn"
-          url="https://www.linkedin.com/in/jolene-kearse-2562ba218/"
-        />
-        <SocialLink
-          img={<FaSquareTwitter />}
-          title="Twitter / X"
-          url="https://x.com/FromJolene"
-        />
-        <SocialLink
-          img={<MdEmail />}
-          title="Email"
-          url="mailto:pharaohnutz@gmail.com"
-        />
-      </ul>
+    <footer className="absolute w-full bottom-2 md:w-5/6">
+      <div className="flex flex-wrap items-center justify-between gap-2">
+        <p className="pr-2 text-sm text-center text-neutral-300">
+          &copy; {new Date().getFullYear()} Meet Jolene. All rights reserved.
+        </p>
+        <ul className="flex gap-3">
+          <SocialLink
+            img={<FaGithub />}
+            title="Github"
+            url="https://github.com/JoleneKearse/"
+          />
+          <SocialLink
+            img={<FaLinkedin />}
+            title="LinkedIn"
+            url="https://www.linkedin.com/in/jolene-kearse-2562ba218/"
+          />
+          <SocialLink
+            img={<FaSquareTwitter />}
+            title="Twitter / X"
+            url="https://x.com/FromJolene"
+          />
+          <SocialLink
+            img={<MdEmail />}
+            title="Email"
+            url="mailto:pharaohnutz@gmail.com"
+          />
+        </ul>
+      </div>
     </footer>
   );
 }
