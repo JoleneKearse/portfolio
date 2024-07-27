b7823758d1894399b00ae6b4baf8d302c3d1b4e7
JoleneKearse <pharaohnutz@gmail.com>
2024-07-27 17:18:05 -0400

    fixes #5: add copy button for email

--- client/src/pages/ContactPage.tsx
+++ client/src/pages/ContactPage.tsx
@@ -1,39 +1,39 @@
 import { Heading } from "../components/Heading";
+import { FaEnvelopeOpenText, FaCopy} from "react-icons/fa6";
 
 export function ContactPage() {
+  const handleCopy = async () => {
+      try {
+        await navigator.clipboard.writeText("pharaohnutz@gmail.com");
+        alert("Jolene's email copied to your clipboard");
+      } catch (err) {
+        alert("Failed to copy to clipboard");
+      }
+    }
+  
   return (
     <section>
       <Heading text="Contact Jolene" />
       <div className="space-y-6">
         <p>
-          Looking for your <span className="emphasis-text">next hire</span>? I'm
-          more than happy to discuss how I am the candidate for you.
+          Looking for your <span className="emphasis-text">next hire</span>? Let me demonstrate how I'm the candidate for you.
         </p>
-        <p>Wanting to collaborate? Let's open a discussion!</p>
-        <p>Either way reach out, I promise I don't bite!</p>
+        <p>Wanting to collaborate? Let's start a conversation!</p>
+        <p>Either way reach out, <i>I promise I don't bite!</i></p>
+        <p>Click the icon to directly message me, or copy my address:</p>
+        <div className="flex items-center justify-between outline">
+          <a href="mailto:pharaohnutz@gmail.com" className="text-purple-500 ">
+            <FaEnvelopeOpenText className="my-6" size="24" title="Email direct from your device" />
+          </a>
+          <div className="flex items-center gap-6 text-purple-500">
+            <p title="I'm quite attached to this email that relates to my early love of Ancient Egyptian history.">pharaohnutz@gmail.com</p>
+            <button onClick={handleCopy}>
+              <FaCopy size="24" title="copy the email" />
+            </button>
+          </div>
+        </div>
       </div>
-      <form className="flex flex-col gap-6 my-20">
-        <input
-          type="text"
-          className="px-4 py-2 bg-purple-800 border-2 border-purple-800 rounded-lg focus:outline-none focus:border-purple-400 focus:border-1 placeholder:text-purple-500"
-          placeholder="Awesome Sauce"
-        />
-        <input
-          type="email"
-          className="p-4 border-2 border-gray-400 rounded-md focus:outline-none focus:border-purple-500"
-          placeholder="Email"
-        />
-        <textarea
-          className="h-32 p-4 border-2 border-gray-400 rounded-md focus:outline-none focus:border-purple-500"
-          placeholder="Message"
-        />
-        <button
-          type="submit"
-          className="px-12 py-3 text-white bg-purple-500 rounded-md hover:bg-purple-400"
-        >
-          Send Message
-        </button>
-      </form>
+      
     </section>
   );
 }
