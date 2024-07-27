e03571bc838051bfcc2571139fa453c88aeb91d1
JoleneKearse <pharaohnutz@gmail.com>
2024-07-27 11:02:37 -0400

    chore: update HomePage copy

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -16,11 +16,14 @@ export function HomePage() {
           />
         </div>
         <div className="mt-20 space-y-6 text-balance md:mt-0 md:flex md:flex-col md:justify-center md:gap-4">
-          <p>I'm a Software Engineer based in Canada specializing in <span className="tech-text">React</span>, <span className="tech-text">JavaScript</span>, and <span className="tech-text">TypeScript</span>.</p>
-          <p className="">Check out my <NavLink to="/skills" className="emphasis-text">Skills</NavLink> to see how I can best help you.</p>
-          <p>Or explore my <NavLink to="/projects" className="emphasis-text">Projects</NavLink> to see I know my stuff.</p>
+          <p>I'm a Frontend Developer based in Canada specializing in <span className="tech-text">React</span>, <span className="tech-text">JavaScript</span>, and <span className="tech-text">TypeScript</span> looking for a remote role.</p>
+          <p>Thank you for checking out my portfolio.  I'll show you that I'm a <span className="emphasis-text">Frontend+ Developer</span>!</p>
         </div>
       </div>
+      <div className="flex flex-col items-center justify-center gap-6">
+        <p className="">My <NavLink to="/skills" className="underline underline-offset-4 emphasis-text">Skills</NavLink> showcase how I can best help you...</p>
+        <p>And my <NavLink to="/projects" className="underline underline-offset-4 emphasis-text">Projects</NavLink> demonstrate that I really know my stuff.</p>
+        </div>
     </section>
   );
 }
