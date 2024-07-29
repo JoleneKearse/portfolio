403e5c6e5285f67258ecf084aa9c406fac4ec63f
JoleneKearse <pharaohnutz@gmail.com>
2024-07-29 15:16:47 -0400

    chore: add repo link and spellcheck to textarea

--- client/src/pages/AdminPage.tsx
+++ client/src/pages/AdminPage.tsx
@@ -4,7 +4,15 @@ export function AdminPage() {
   return (
     <>
       <Heading text="Update Projects" />
-      <form className="space-y-8 text-neutral-300 font-roboto-mono">
+      <a
+        href="https://github.com/JoleneKearse?tab=repositories"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="font-alegreya font-extrabold text-2xl text-purple-500 text-center w-full mx-auto block hover:text-purple-400"
+      >
+        Repos
+      </a>
+      <form className="space-y-8 text-neutral-300 font-roboto-mono py-10">
         <input
           type="text"
           name=""
@@ -19,7 +27,13 @@ export function AdminPage() {
           className="bg-purple-800 py-2 px-4 rounded-lg w-full placeholder:text-neutral-600 hover:shadow-inner hover:shadow-purple-500 border-none hover:shadow-xl focus:shadow-inner focus:shadow-purple-400 focus:shadow-xl focus:outline-none"
           placeholder="Image"
         />
-        <input type="text" name="" id="video" className="bg-purple-800 py-2 px-4 rounded-lg w-full placeholder:text-neutral-600 hover:shadow-inner hover:shadow-purple-500 border-none hover:shadow-xl focus:shadow-inner focus:shadow-purple-400 focus:shadow-xl focus:outline-none" placeholder="Video Link" />
+        <input
+          type="text"
+          name=""
+          id="video"
+          className="bg-purple-800 py-2 px-4 rounded-lg w-full placeholder:text-neutral-600 hover:shadow-inner hover:shadow-purple-500 border-none hover:shadow-xl focus:shadow-inner focus:shadow-purple-400 focus:shadow-xl focus:outline-none"
+          placeholder="Video Link"
+        />
         <input
           type="text"
           name=""
@@ -37,10 +51,16 @@ export function AdminPage() {
         <textarea
           rows={5}
           id="description"
+          spellCheck="true"
           className="bg-purple-800 py-2 px-4 rounded-lg w-full placeholder:text-neutral-600 hover:shadow-inner hover:shadow-purple-500 border-none hover:shadow-xl focus:shadow-inner focus:shadow-purple-400 focus:shadow-xl focus:outline-none"
           placeholder="What problems does the project solve?"
         ></textarea>
-        <select name="" id="techUsed" className="bg-purple-800 w-2/5 outline-none hover:shadow-inner hover:shadow-purple-500 border-none hover:shadow-xl p-2 block mx-auto" multiple>
+        <select
+          name=""
+          id="techUsed"
+          className="bg-purple-800 w-2/5 outline-none hover:shadow-inner hover:shadow-purple-500 border-none hover:shadow-xl p-2 block mx-auto"
+          multiple
+        >
           <option value="">techUsed</option>
           <option value="React">React</option>
           <option value="JavaScript">JavaScript</option>
@@ -64,7 +84,12 @@ export function AdminPage() {
           <option value="HTML5">HTML</option>
           <option value="Linux">Linux</option>
         </select>
-        <button className="block mx-auto bg-gradient-to-br from-pink-600 via-purple-500 to-purple-400 py-2 px-4 rounded-2xl text-purple-950 font-semibold hover:bg-gradient-to-tr" type="button">Add Project</button>
+        <button
+          className="block mx-auto bg-gradient-to-br from-pink-600 via-purple-500 to-purple-400 py-2 px-4 rounded-2xl text-purple-950 font-semibold hover:bg-gradient-to-tr"
+          type="button"
+        >
+          Add Project
+        </button>
       </form>
     </>
   );
