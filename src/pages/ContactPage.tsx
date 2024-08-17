f8937d3d7eecafce79997d85f47e7c9a264f33d0
JoleneKearse <pharaohnutz@gmail.com>
2024-08-17 10:29:02 -0400

    chore: add larger ctas for easier contact

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -1,5 +1,5 @@
 import { Heading } from "../components/Heading";
-import { FaEnvelopeOpenText, FaCopy } from "react-icons/fa6";
+import { FaEnvelopeOpenText, FaCopy, FaLinkedin } from "react-icons/fa6";
 import Jolene from "/jolene.webp";
 
 export function ContactPage() {
@@ -15,26 +15,21 @@ export function ContactPage() {
   return (
     <section>
       <Heading text="Contact Jolene" />
-      <div className="grid md:grid-cols-2">
-        <div className="space-y-6">
+      <div className="grid grid-cols-2 gap-6 font-normal mb-28">
+        <div className="rounded-xl bg-purple-600 p-10">
           <p>
-            Looking for your <span className="emphasis-text">next hire</span>?
-            Let me demonstrate how I'm the candidate for you.
+            Click the email icon to message me directly or the copy button if
+            you're on a public device.
           </p>
-          <p>Wanting to collaborate? Let's start a conversation!</p>
-          <p>
-            Either way reach out, <i>I promise I don't bite!</i>
-          </p>
-          <p>Click the icon to directly message me, or copy my address:</p>
-          <div className="flex items-center justify-between">
-            <a href="mailto:pharaohnutz@gmail.com" className="text-purple-500 ">
+          <div className="flex flex-col gap-2 justify-center items-center">
+            <a href="mailto:pharaohnutz@gmail.com" className="text-purple-900">
               <FaEnvelopeOpenText
                 className="my-6 hover:text-neutral-500"
-                size="24"
+                size="36"
                 title="Email direct from your device"
               />
             </a>
-            <div className="flex items-center gap-6 text-purple-500 hover:text-neutral-500">
+            <div className="flex items-center gap-2 text-purple-900 hover:text-neutral-500">
               <p title="I'm quite attached to this email that relates to my early love of Ancient Egyptian history.">
                 pharaohnutz@gmail.com
               </p>
@@ -44,8 +39,41 @@ export function ContactPage() {
             </div>
           </div>
         </div>
-        
-        <div className="w-1/2 mx-auto my-auto rounded-s-full bg-neutral-400 md:w-3/5">
+        <div className="space-between flex flex-col space-y-6 rounded-xl bg-pink-700 p-10">
+          <p>Or connect / DM me on LinkedIn.</p>
+          <p className="font-light">And check out my daily videos!</p>
+          <a
+            href="https://www.linkedin.com/in/jolene-kearse-2562ba218/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mt-6 text-center text-purple-900 hover:text-neutral-500 flex justify-center"
+          >
+            <FaLinkedin size="36" />
+          </a>
+        </div>
+      </div>
+      <div className="grid md:grid-cols-2">
+        <div className="space-y-6">
+          <p>
+            Looking for your <span className="emphasis-text">next hire</span>?
+            I'm in your candidate! I'm all set to add value to your team. And
+            I'll be there for the long-term.
+          </p>
+          <p>
+            Do you have a project you'd like a little extra help with? I'm ready
+            and willing to <span className="emphasis-text">freelance</span>.
+            Let's get your idea out there lickety-split!
+          </p>
+          <p>
+            Wanting to <span className="emphasis-text">collaborate</span>? I
+            love working with other devs. Let's get those ideas rolling!
+          </p>
+          <p>
+            Either way reach out, <i>I promise I don't bite!</i>
+          </p>
+        </div>
+
+        <div className="mx-auto my-auto w-1/2 rounded-s-full bg-neutral-400 md:w-3/5">
           <img
             src={Jolene}
             alt="Jolene Kearse, Frontend and Software Engineer"
