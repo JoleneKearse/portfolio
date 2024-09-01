9ed2f8fb778e92bdd578c98a93f4b07258ca86be
JoleneKearse <pharaohnutz@gmail.com>
2024-09-01 16:56:29 -0400

    chore: add function to obtain and add image file url

--- src/services/projectActions.ts
+++ src/services/projectActions.ts
@@ -8,7 +8,7 @@ export async function addProject(projectData: Omit<Project, "id">) {
 }
 
 export async function handleFileUpload(file: File, bucketName: string): Promise<string | null> {
-  const { data, error } = await supabase.storage
+  const { error } = await supabase.storage
     .from(bucketName)
     .upload(file.name, file);
 
@@ -18,5 +18,29 @@ export async function handleFileUpload(file: File, bucketName: string): Promise<
     return null;
   }
 
-  return data?.path || null;
+  // generate url to save in db
+  return getFileUrl(bucketName, file.name);
+}
+
+export async function getFileUrl(bucketName: string, fileName: string): Promise<string | null> {
+  const { data } = await supabase.storage
+    .from(bucketName)
+    .getPublicUrl(fileName);
+  return data.publicUrl;
+}
+
+export async function getProjects(): Promise<Project[]> {
+  const { data, error } = await supabase
+    .from("projects")
+    .select("*");
+
+  if (error) {
+    console.error("Error getting projects:", error);
+    alert(`Error getting projects: ${error.message}`);
+    return [];
+  }
+
+  console.log("fetched projects from ProjectAction function:", data);
+
+  return data || [];
 }
