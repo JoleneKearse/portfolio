5ff2090dc8638815587fa5bd901f48862695ebf9
JoleneKearse <pharaohnutz@gmail.com>
2026-03-25 12:22:02 -0400

    Populate ProjectsPage and ProjectsDetailPage from Supabase db Fixes #13

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,20 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 
 import { NavBar } from "./NavBar";
 import { Footer } from "./Footer";
 
 export function Layout() {
+  const { pathname } = useLocation();
+  const isProjectsListPage = pathname === "/projects";
+
   return (
     <div className="px-2 py-4 md:px-6 lg:px-20 md:py-6 lg:py-10">
       <NavBar />
-      <main className="mx-auto my-20 max-w-prose">
+      <main
+        className={`mx-auto my-20 w-full ${
+          isProjectsListPage ? "max-w-5xl" : "max-w-prose"
+        }`}
+      >
         <Outlet />
       </main>
       <Footer />
